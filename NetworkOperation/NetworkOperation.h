1683d92fa8e80c966e77e0064a77222bba33d14b
Alasdair Law <alasdair.law@icloud.com>
2016-12-17 22:08:16 +0000

    Create project

---
+++ NetworkOperation/NetworkOperation.h
@@ -0,0 +1,19 @@
+//
+//  NetworkOperation.h
+//  NetworkOperation
+//
+//  Created by Alasdair Law on 17/12/2016.
+//  Copyright © 2016 Alasdair Law. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkOperation.
+FOUNDATION_EXPORT double NetworkOperationVersionNumber;
+
+//! Project version string for NetworkOperation.
+FOUNDATION_EXPORT const unsigned char NetworkOperationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkOperation/PublicHeader.h>
+
+
